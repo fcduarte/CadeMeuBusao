10743a020e0b8c139b73dfb76acdd5c9f893c1a3
arctouch-felipeduarte <felipe.duarte@arctouch.com>
2012-06-27 11:20:56 -0300

    test

--- src/br/com/cademeubusao/servlet/OnibusServlet.java
+++ src/br/com/cademeubusao/servlet/OnibusServlet.java
@@ -16,6 +16,9 @@ public class OnibusServlet extends HttpServlet {
 	
 	private static final Logger log = Logger.getLogger(OnibusServlet.class.getName());
 	 
+	/* (non-Javadoc)
+	 * @see javax.servlet.http.HttpServlet#doPost(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
+	 */
 	public void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		EntityManager em = null;
